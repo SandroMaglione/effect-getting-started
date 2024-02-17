9f1354c1567b30e9356a8c6db73555a7956e5c8d
SandroMaglione <lass.maglio@gmail.com>
2024-02-17 11:47:05 +0100

    `file` gzip download service

---
+++ src/jmdict/file.ts
@@ -0,0 +1,97 @@
+import * as Fs from "@effect/platform/FileSystem";
+import { Context, Data, Effect } from "effect";
+import * as NFS from "fs";
+import * as http from "http";
+
+// @ts-expect-error
+import gunzip from "gunzip-file";
+
+class FileError extends Data.TaggedError("FileError")<{
+  error: unknown;
+}> {}
+
+export const downloadFolder = "./.download/";
+
+const make = {
+  download: ({
+    toFilename,
+    url,
+    useCachedFile,
+  }: {
+    readonly url: string;
+    readonly toFilename: string;
+    readonly useCachedFile: boolean;
+  }) =>
+    Effect.gen(function* (_) {
+      const fs = yield* _(Fs.FileSystem);
+      const existsFilename = yield* _(fs.exists(toFilename));
+      if (useCachedFile && existsFilename) {
+        return yield* _(Effect.logDebug(`using cached file "${toFilename}"`));
+      }
+
+      const existsDownloadFolder = yield* _(fs.exists(downloadFolder));
+
+      if (!existsDownloadFolder) {
+        yield* _(Effect.logDebug(`creating folder ${downloadFolder}...`));
+        yield* _(fs.makeDirectory(downloadFolder));
+      }
+
+      yield* _(Effect.logDebug(`downloading ${toFilename}...`));
+
+      return yield* _(
+        // TODO: Convert this to Effect (How to?)
+        Effect.tryPromise({
+          try: () =>
+            new Promise<void>((resolve, reject) => {
+              const writeStream = NFS.createWriteStream(toFilename);
+
+              const request = http.get(url, (response) => {
+                response.pipe(writeStream);
+                writeStream.on("finish", () => {
+                  writeStream.close(() => resolve());
+                });
+              });
+
+              request.on("error", async (err) => {
+                NFS.unlinkSync(downloadFolder + toFilename);
+                reject(err.message);
+              });
+            }),
+          catch: (error) => new FileError({ error }),
+        })
+      );
+    }),
+
+  extractGzip: ({
+    extractedFilename,
+    gzipFilename,
+    useCachedFile,
+  }: {
+    readonly gzipFilename: string;
+    readonly extractedFilename: string;
+    readonly useCachedFile: boolean;
+  }) =>
+    Effect.gen(function* (_) {
+      const fs = yield* _(Fs.FileSystem);
+      const existsExtractedFilename = yield* _(fs.exists(extractedFilename));
+      if (useCachedFile && existsExtractedFilename) {
+        return yield* _(
+          Effect.logDebug(`using cached file "${extractedFilename}"`)
+        );
+      }
+
+      yield* _(Effect.logDebug(`extracting ${gzipFilename}...`));
+      yield* _(
+        // TODO: Effect.async
+        Effect.tryPromise({
+          try: () =>
+            new Promise<void>((resolve, _) => {
+              gunzip(gzipFilename, extractedFilename, () => resolve());
+            }),
+          catch: (error) => new FileError({ error }),
+        })
+      );
+    }),
+};
+
+export class File extends Context.Tag("File")<File, typeof make>() {}

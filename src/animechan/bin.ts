65c656b74f958992804564b9d7a068acf70c5661
SandroMaglione <lass.maglio@gmail.com>
2024-03-02 18:12:25 +0900

    execute api request

--- src/animechan/bin.ts
+++ src/animechan/bin.ts
@@ -1,9 +1,20 @@
-import { Config, Layer } from "effect";
+import { Config, Console, Effect, Layer } from "effect";
 import * as Api from "./Api.js";
 import * as Client from "./Client.js";
 
 const ClientLive = Client.Client.Live({
-  baseUrl: Config.string("baseUrl"),
+  baseUrl: Config.string("BASE_URL"),
 });
 
 const MainLive = Api.Api.Live.pipe(Layer.provide(ClientLive));
+
+const program: Effect.Effect<void, never, never> = Effect.gen(function* (_) {
+  const api = yield* _(Api.Api);
+  return yield* _(api.random);
+}).pipe(
+  Effect.provide(MainLive),
+  Effect.flatMap((quote) => Console.log(quote)),
+  Effect.catchAll((error) => Console.error(error))
+);
+
+Effect.runFork(program);
